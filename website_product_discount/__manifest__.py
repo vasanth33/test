9f7e2fab9c79245583b6b708045007f17603f3a3
vasanth <vasanth.radhakrishnan@rminds.com>
2024-10-08 16:37:56 +0530

    added screenshots in description

--- website_product_discount/__manifest__.py
+++ website_product_discount/__manifest__.py
@@ -5,7 +5,7 @@
     'version': '17.0',
     'category': '',
     "sequence": 3,
-    'summary': "This module is used for a Product Discount Feature where a percentage discount can be applied to specific products, and the discounted price should be reflected in the product listings and on the product detail page in the eCommerce store",
+    'summary': "This module is used for a Product Discount Feature where a percentage discount can be applied to specific products, and the discounted price should be reflected in the product listings and on the product detail page in the eCommerce store""",
     "license": "LGPL-3",
     'author': 'Rminds Inc.',
     'depends': ["base",'website_sale','product'],
