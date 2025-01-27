86f999d4183dc33ab6035d2966162eb691cb1ce5
vasanth33 <vsnthajay@gmail.com>
2025-01-27 17:01:22 +0530

    author change

--- website_product_discount/__manifest__.py
+++ website_product_discount/__manifest__.py
@@ -2,12 +2,12 @@
 
 {
     'name': "Website Product Discount",
-    'version': '17.0',
+    'version': '18.0',
     'category': '',
     "sequence": 3,
     'summary': "This module is used for a Product Discount Feature where a percentage discount can be applied to specific products, and the discounted price should be reflected in the product listings and on the product detail page in the eCommerce store""",
     "license": "LGPL-3",
-    'author': 'Rminds Inc.',
+    'author': 'Vasanth',
     'depends': ["base",'website_sale','product'],
     'data': [
         "views/product_template_view.xml",
